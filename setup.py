02b1e44516f92d2a442a05715cfe7656a91667e7
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-04-20 14:14:55 +0200

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="automata",
+      version="0.0.1",
+      description="A library for Finite Automaton manipulation",
+      author="pjdevs",
+      author_email="pj33720@gmail.com",
+      packages=["automata", "tests"],
+      install_requires=[],
+      extras_require={},
+      license="MIT"
+)
