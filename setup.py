671f3e96f12bfdbacda108c4dc79a4f3a0cd211c
MOREL Pierre-Jean <pimorel@enseirb-matmeca.fr>
2021-04-20 14:33:21 +0200

    Fix setup file

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="automata",
       version="0.0.1",
       description="A library for Finite Automaton manipulation",
       author="pjdevs",
       author_email="pj33720@gmail.com",
-      packages=["automata, automata.utils, automata.tests"],
+      packages=find_packages(),
       install_requires=[],
       extras_require={},
       license="MIT"
